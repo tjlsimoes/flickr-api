d4cc77e65391f54b25f77f8f6e80382725584d86
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-30 15:23:11 +0000

    Implement search/display for user_id's photos

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
+  @@flickr = Flickr.new(Figaro.env.flickr_api_key, Figaro.env.flickr_secret)
   def index
+    unless params[:user_id].nil?
+      @photos = @@flickr.people.getPublicPhotos user_id: params[:user_id]
+    end
   end
 end
