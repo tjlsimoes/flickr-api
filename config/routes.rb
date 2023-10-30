e43757fcfca96b65ce510de096fb25e6a1cc0421
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-30 12:14:59 +0000

    Set up root to search form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "static_pages/", to: "static_pages#index"
+  root "static_pages#index"
 end
